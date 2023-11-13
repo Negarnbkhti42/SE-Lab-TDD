4a532c91b18e475a9749efa41f34c60136186a64
parimehrmorassa <62252489+parimehrmorassa@users.noreply.github.com>
2023-11-13 23:43:04 +0330

    classes

--- src/main/java/org/example/Square.java
+++ src/main/java/org/example/Square.java
@@ -1,5 +1,25 @@
 package org.example;
 
 public class Square {
+    private int side;
 
+    public Square(int side) {
+        setSide(side);
+    }
+
+    public int getSide() {
+        return side;
+    }
+
+    public void setSide(int side) {
+        if (side >= 0) {
+            this.side = side;
+        } else {
+            throw new IllegalArgumentException("Side must be a non-negative integer");
+        }
+    }
+
+    public int computeArea() {
+        return side * side;
+    }
 }
